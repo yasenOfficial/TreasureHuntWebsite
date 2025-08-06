22beb7f76f471c5e94b9671be6e75a77cbeb13d5
yasenOfficial <yasentsv@gmail.com>
2025-08-06 23:36:18 +0300

    fresh start

--- client/static/js/clearLocalStorage.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function clearLocalStorage() {
-    // Clear all items from local storage
-    localStorage.clear();
-    console.log("Local storage has been cleared.");
-});
